d18f03cb0a3d371a2f9bc3e30a9177d75a4cf174
unknown <abdullahalrahman23@gmail.com>
2024-12-21 12:41:06 +0300

    add:setup branch

---
+++ TicketBooking.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class TicketBook {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        System.out.println("Hellow " + number);
+    }
+}
