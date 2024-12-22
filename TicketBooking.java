6175489ad10a7b5d56c78b78b9e35d40145c2356
savanb654@gmail.com <savanb654@gmail.com>
2024-12-22 21:34:04 +0300

    i made a switch statement and if statement for the first option so it works perfectly

--- TicketBooking.java
+++ TicketBooking.java
@@ -3,27 +3,73 @@ class TicketBook {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         //1. viewing the options
-        /*but firstly we need a for loop so the options will be shown 
+        /*but firstly we need an infinite for loop so the options will be shown 
         until exist is chosen*/
         for(;;){
+            
         System.out.println("-----+-----+-----");
+        System.out.println("");
         System.out.println("1.Select the flight class              6.view current status of the plane seating arrangement");
         System.out.println("2.Book a seat                          7.View list of seats reserved");
         System.out.println("3.view the booked e-ticket information 8.display the status of the seat");
         System.out.println("4.view lists of all booked seats       9.view a report of the booked seats");
         System.out.println("5.cancel your flight                   10.exist");
 
+
+        System.out.println("");
         //2. asking the user to enter their choice:
         System.out.println("-----+-----+-----");
         System.out.print("Please, Enter your choice: ");
         int choiceNumber = input.nextInt();
         System.out.println("-----+-----+-----");
+        System.out.println("");
+        //3. we are making a switch statement in order to go through the options according to the users choice
          switch(choiceNumber){
-
-            case 1:
             
+            //case no.1 goes through the classes and their prices 
+            case 1:
+            //we call the flightClass method because the operations are done in that method
+            flightClasses();
          }
 
         }
+}
+
+//this method is dedicated to case no.1
+public static void flightClasses(){
+   
+    //we spcify the prices of each class
+    int economyClass = 100000;
+    int businessClass = 200000;
+    int firstClass = 300000;
+    int roundTrip = 100000;
+
+    //we print the prices for the user to see
+    System.out.println("Economy class is 100,000IQD");
+    System.out.println("Business class is 200,000IQD");
+    System.out.println("First class class is 300,000IQD");
+    System.out.println("and additional roundtrip is 100,000IQD");
+
+    //here we ask the user to enter their class choice
+    System.out.print("Write 'a' for Economy class, write 'b' for business class, or 'c' for first class: ");
+    char theClass = input.next().charAt(0);
+    
+    //we make if-else statements to differentiate between each class
+    if(theClass == 'a'){
+        System.out.println("the total price with additional roundtrip is " + (economyClass+roundTrip) + " IQD");
+    }
+    else if(theClass == 'b'){
+        System.out.println("the total price with additional roundtrip is " + (businessClass+roundTrip) + " IQD");
+    }
+    else if(theClass == 'c'){
+        System.out.println("the total price with additional roundtrip is " + (firstClass+roundTrip) + " IQD");
+    }
+
+    //if the user tries to write any letter other than the valid ones he/she will get an error
+    else{
+        System.out.println("invalid letter, try again...");
+    }
+
+    System.out.println("");
 }
 }
