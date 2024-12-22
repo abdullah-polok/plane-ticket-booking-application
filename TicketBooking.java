7f7e49387c2d806bec357a29accbed263ba37520
savanb654@gmail.com <savanb654@gmail.com>
2024-12-22 13:15:33 +0300

    i made a for loop and a switch

--- TicketBooking.java
+++ TicketBooking.java
@@ -5,22 +5,25 @@ class TicketBook {
         //1. viewing the options
         /*but firstly we need a for loop so the options will be shown 
         until exist is chosen*/
-        
+        for(;;){
         System.out.println("-----+-----+-----");
         System.out.println("1.Select the flight class              6.view current status of the plane seating arrangement");
         System.out.println("2.Book a seat                          7.View list of seats reserved");
         System.out.println("3.view the booked e-ticket information 8.display the status of the seat");
         System.out.println("4.view lists of all booked seats       9.view a report of the booked seats");
         System.out.println("5.cancel your flight                   10.exist");
-        
-        
-       
+
         //2. asking the user to enter their choice:
         System.out.println("-----+-----+-----");
         System.out.print("Please, Enter your choice: ");
         int choiceNumber = input.nextInt();
         System.out.println("-----+-----+-----");
+         switch(choiceNumber){
 
-        
+            case 1:
+            
+         }
+
+        }
 }
 }
